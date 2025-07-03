956e819b8f8bdd4a12917aa61427229503784e43
Bantupalli Bhaskara Rao <127121617+Bhaskar-Rao-1@users.noreply.github.com>
2025-07-03 06:59:08 +0530

    Create TeacherHome.jsx

---
+++ Project Files/frontend/src/components/user/teacher/TeacherHome.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TeacherHome = () => {
+  return (
+    <div>
+      <h2>Teacher Dashboard</h2>
+      <p>Upload and manage your courses here.</p>
+    </div>
+  );
+};
+
+export default TeacherHome;
